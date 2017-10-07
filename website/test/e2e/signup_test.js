6f2031e78346c212c9ba838b587e7fd31e56265c
Neil Edwards <neil@plasticwater.com>
2017-10-06 22:38:37 -0400

    test for cancel signup

--- website/test/e2e/signup_test.js
+++ website/test/e2e/signup_test.js
@@ -4,8 +4,16 @@ Feature('Sign Up')
 Scenario('Should see the sign up form', (I) => {
   I.amOnPage('/')
   I.click('Sign Up')
+  I.seeInCurrentUrl('/signup')
   I.see('Email')
   I.see('Username')
   I.see('Password')
   I.see('Password Confirmation')
+})
+
+Scenario('Should be taken back to the main page if I cancel', (I) => {
+  I.amOnPage('/')
+  I.click('Sign Up')
+  I.click('Cancel')
+  I.see('Welcome')
 })
